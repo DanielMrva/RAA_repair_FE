e28f092f220617672ccbe99d143ca952db8e7a16
Daniel Mrva <mrva.paints@gmail.com>
2024-05-13 16:10:39 -0600

    Error fixed

--- src/app/services/radios/radio.dataSource.ts
+++ src/app/services/radios/radio.dataSource.ts
@@ -33,7 +33,7 @@ export class RadioDataSource extends DataSource<Radio> {
     loadAllRadios(): void {
         this.isLoading$.next(true);
         this.radioService.allRadios().valueChanges.subscribe(( {data} ) => {
-            this.radios$.next(data.radios);
+            this.radios$.next(data.allRadios);
             this.isLoading$.next(false);
         });
     }
